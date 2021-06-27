623b55b6b1edb971bdf227d4ebce88481ebd6913
MykolaSaviuk <kolya0603123@gmail.com>
2021-06-27 17:59:25 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express');
+const sequelize = require('./db')
+const model = require('./model/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const  router = require('./routes/index')
+const errorHandler = require('./middeleware/ErrorHandingMiddleware')
+
+const  PORT = process.env.PORT || 5000
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload({}))
+app.use('/api', router)
+
+
+app.use(errorHandler)
+
+
+
+const start = async () => {
+    try {
+       await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on part ${PORT}`))
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+start();
+
+
